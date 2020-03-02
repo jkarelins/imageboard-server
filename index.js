6a5a3ab88671f7337dc874fb8614c6f75a3def15
jkarelins <jkarelins@gmail.com>
2020-03-02 10:41:52 +0100

    model

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
+// const db = require("./db");
+const Image = require("./models/model");
 
 // app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () =>
