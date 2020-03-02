583f92643c14958b17700c853c1aa5cdbc1b33d0
jkarelins <jkarelins@gmail.com>
2020-03-02 13:36:33 +0100

    login functionality

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ const corsMiddleware = cors();
 const parserMiddleware = bodyParser.json();
 const Image = require("./models/model");
 const imageRoute = require("./models/router");
+const loginRoute = require("./auth/router");
 
 app.use(corsMiddleware);
 app.use(parserMiddleware);
 app.use("/image", imageRoute);
+app.use(loginRoute);
 
 app.listen(port, () =>
   console.log(`ImageBoard app listening on port ${port}!`)
