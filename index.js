2c7d72b2bb5685669dcf004dc525ce2240e619dc
jkarelins <jkarelins@gmail.com>
2020-03-03 10:44:05 +0100

    cors

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 4000;
-
+const cors = require("cors");
 const corsMiddleware = cors();
+app.use(corsMiddleware);
+const bodyParser = require("body-parser");
 const parserMiddleware = bodyParser.json();
 const imageRoute = require("./models/router");
 const loginRoute = require("./auth/router");
 
-app.use(corsMiddleware);
 app.use(parserMiddleware);
 app.use("/image", imageRoute);
 app.use(loginRoute);
